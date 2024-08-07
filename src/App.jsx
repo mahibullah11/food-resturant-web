6c09d6606580761563cca124f5e3e249b4081146
Mahiballah <145888808+mahibullah11@users.noreply.github.com>
2024-08-07 21:54:48 +0500

    done with header and herocomponent

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+import HeroComponent from "./components/HeroComponent"
+function App() {
+
+
+  return (
+    <>
+    <Header/>
+    <HeroComponent/>
+    </>
+
+  )
+}
+
+export default App
